e85d4efcf0e26ff877ca0b4738b10b5784fc910b
Nikita Sandhu <sandhunikita0096@gmail.com>
2020-02-19 17:52:13 -0500

    Initial Commit

---
+++ ReadTextJSON/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ReadTextJSON
+//
+//  Created by Nikita Sandhu on 2020-02-19.
+//  Copyright © 2020 Nikita Sandhu. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
